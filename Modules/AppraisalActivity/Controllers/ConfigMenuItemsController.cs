fb315e434db9f5ba5283d4ea5f9a2ccd73797a9b
roy nsumba <83820529+roynsumba@users.noreply.github.com>
2024-07-30 18:15:59 +0300

    feat: Added ConfigMenuItemService.

---
+++ Modules/AppraisalActivity/Controllers/ConfigMenuItemsController.cs
@@ -0,0 +1,37 @@
+﻿using AppraisalTracker.Modules.AppraisalActivity.Models;
+using AppraisalTracker.Modules.AppraisalActivity.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppraisalTracker.Modules.AppraisalActivity.Controllers
+{
+    [Route("api/ConfigMenuItems")]
+    [ApiController]
+    public class ConfigMenuItemsController : ControllerBase
+    {
+
+        private readonly IConfigMenuItemService _configMenuItemService;
+        public ConfigMenuItemsController(IConfigMenuItemService configMenuItemService)
+        {
+            _configMenuItemService = configMenuItemService;
+        }
+
+    
+        [HttpGet("GetConfigMenuItems")]
+        public async Task<ActionResult<ConfigMenuItem>> GetConfigMenuItems()
+        {
+            var configMenuItems = await _configMenuItemService.FetchConfigMenuItems();
+            return Ok(configMenuItems);
+        }
+
+        [HttpPost("CreateConfigMenuItem")]
+        public async Task<ConfigMenuItem> PostConfigMenuItem(ConfigMenuItem configMenuItem)
+        {
+            return await _configMenuItemService.PostConfigMenuItem(configMenuItem);
+
+        }
+
+
+
+    }
+}
---
+++ Modules/AppraisalActivity/Services/ConfigMenuItemService.cs
@@ -0,0 +1,41 @@
+﻿using AppraisalTracker.Data;
+using AppraisalTracker.Exceptions;
+using AppraisalTracker.Modules.AppraisalActivity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppraisalTracker.Modules.AppraisalActivity.Services
+{
+    public interface IConfigMenuItemService
+    {
+        public Task<List<ConfigMenuItem>> FetchConfigMenuItems();
+        public Task<ConfigMenuItem> PostConfigMenuItem(ConfigMenuItem configmenuitem);
+    }
+    public class ConfigMenuItemService : IConfigMenuItemService
+    {
+
+        public readonly AppDbContext _context;
+        public ConfigMenuItemService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ConfigMenuItem>> FetchConfigMenuItems()
+        {
+            return await _context.ConfigMenuItems.ToListAsync();
+        }
+
+        public async Task<ConfigMenuItem> PostConfigMenuItem(ConfigMenuItem configmenuitem)
+        {
+            if (_context.ConfigMenuItems.Any(e => e.ItemId == configmenuitem.ItemId))
+            {
+                throw new ClientFriendlyException("Implementation already exisitng");
+            }
+
+            _context.ConfigMenuItems.Add(configmenuitem);
+            await _context.SaveChangesAsync();
+
+            return configmenuitem;
+        }
+
+    }
+}
