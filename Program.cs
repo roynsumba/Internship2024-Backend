8cf6e9291f84cc15a72c46dec4e0b9ea08f26e08
roy nsumba <83820529+roynsumba@users.noreply.github.com>
2024-07-30 18:32:24 +0300

    chore: necessary program.cs mod.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IAppraisalActivityService, AppraisalActivityService>();
+builder.Services.AddScoped<IConfigMenuItemService, ConfigMenuItemService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
