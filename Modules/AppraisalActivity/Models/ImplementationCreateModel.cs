8c59a284a61b3799274c8486d5ba7f0c3202c899
Wamoto <twamoto@ura.go.ug>
2024-08-01 12:07:32 +0300

    chore: deleted unused "usings" in the implementation create model.

--- Modules/AppraisalActivity/Models/ImplementationCreateModel.cs
+++ Modules/AppraisalActivity/Models/ImplementationCreateModel.cs
@@ -1,13 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
-
-namespace AppraisalTracker.Modules.AppraisalActivity.Models
+﻿namespace AppraisalTracker.Modules.AppraisalActivity.Models
 {
     public class ImplementationCreateModel
     {
 
-        
         public string Description { get; set; } = string.Empty;
         public string? Comment { get; set; }
         public string? Stakeholder { get; set; }
