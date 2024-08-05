3c12359ab63564816548e332c7e2594a5ce1f26d
Wamoto <twamoto@ura.go.ug>
2024-08-05 17:15:04 +0300

    refactor: renamed User "Models" folder

--- Modules/Users/Models/LoginModel.cs
+++ Modules/Users/Models/LoginModel.cs
@@ -1,13 +1,16 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace AppraisalTracker.Modules.Login
 {
-    public class LoginModel{
+    public class LoginModel
+    {
         [Required]
-        
+
         public required string Username { get; set; }
-        
+
         [Required]
+        [PasswordPropertyText]
         public required string Password { get; set; }
     }
 }
